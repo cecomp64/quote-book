65ec01502813cb5f2236527b14edd68c453710d5
Carl Svensson <csvenss2@gmail.com>
2017-06-10 11:02:45 -0700

    Seed!

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610164906) do
+ActiveRecord::Schema.define(version: 20170610174856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 20170610164906) do
     t.integer  "author"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "score"
+  end
+
+  add_index "quotes", ["attribution"], name: "index_quotes_on_attribution", using: :btree
+  add_index "quotes", ["author"], name: "index_quotes_on_author", using: :btree
+  add_index "quotes", ["score"], name: "index_quotes_on_score", using: :btree
+
+  create_table "quotes_votes", id: false, force: true do |t|
+    t.integer "quote_id", null: false
+    t.integer "vote_id",  null: false
   end
 
   create_table "users", force: true do |t|
@@ -47,5 +57,21 @@ ActiveRecord::Schema.define(version: 20170610164906) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "users_votes", id: false, force: true do |t|
+    t.integer "user_id", null: false
+    t.integer "vote_id", null: false
+  end
+
+  create_table "votes", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "quote_id"
+    t.integer  "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "votes", ["quote_id"], name: "index_votes_on_quote_id", using: :btree
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id", using: :btree
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,24 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 quotes = [
-    {text: 'That was a good one!', author: 'CE', attribution: 'CE'},
+    {text: 'That was a good one!', author: 'CE', attribution: 'CE', score: rand(100)},
+    {text: 'Now I have 14,000 poop samples.', author: 'Leah', attribution: nil, score: rand(100)},
+    {text: 'My friend had this quote book, and it was awesome.', author: 'CE', attribution: 'Kynan', score: rand(100)},
+    {text: 'Four score, and seven years ago...', author: 'Anya', attribution: 'Abraham Lincoln', score: rand(100)},
+    {text: "That's what she said!", author: 'CE', attribution: 'Michael Scott', score: rand(100)},
+    {text: 'Time keeps on ticking... into the future.', author: 'Anya', attribution: nil, score: rand(100)},
+    {text: 'How many roads must a man walk down?', author: 'Anya', attribution: 'Bob Dylan', score: rand(100)},
+    {text: 'Giant lingham.', author: 'Anya', attribution: 'Shiva The Destroyer', score: rand(100)},
+    {text: "Yo, I'll tell you what I want, what I really, really want.", author: 'CE', attribution: 'Spice Girl One', score: rand(100)},
+    {text: 'So, tell me what you want, what you really, really want.', author: 'Anya', attribution: 'Spice Girl Two', score: rand(100)},
+    {text: 'Ai-ya-ya-ya-ya, I keep on hoping there will be cake by the ocean - Hu!', author: 'Anya', attribution: 'Some Band', score: rand(100)},
+    {text: "It's morphin' time!", author: 'Anya', attribution: 'Jason, the Red Ranger', score: rand(100)},
 ]
+
+anonymous = Person.find_or_create_by(name: 'Anonymous')
+
+quotes.each do |quote|
+  author = quote[:author] ? Person.find_or_create_by(name: quote[:author]) : anonymous
+  attribution = quote[:attribution] ? Person.find_or_create_by(name: quote[:attribution]) : anonymous
+  Quote.create(text: quote[:text], author: author, attribution: attribution, score: quote[:score])
+end

a224814d654c34a770e864bf24b8e35612378372
Carl Svensson <csvenss2@gmail.com>
2017-06-10 11:48:51 -0700

    Some dope styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def display_person(p)
+    return p.name if(p)
+  end
 end
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,6 +3,6 @@ class Quote < ActiveRecord::Base
   belongs_to :author, class_name: 'Person', foreign_key: :author
   has_and_belongs_to_many :votes
 
-  validates_presence_of :text
+  validates_presence_of :text, :author, :attribution
   validates_uniqueness_of :text, case_sensitive: false
 end

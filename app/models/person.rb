c72723a059e9bed5337e22bd931e8a380fe33adf
Carl Svensson <csvenss2@gmail.com>
2017-06-10 11:03:29 -0700

    habtm votes

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,7 @@
 class Person < ActiveRecord::Base
   has_many :quotes, foreign_key: :attribution
   has_many :entries, class_name: 'Quote', foreign_key: :author
+  has_and_belongs_to_many :votes
 
   validates_presence_of :name
   validates_uniqueness_of :name, case_sensitive: false
--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,7 @@
 class Quote < ActiveRecord::Base
   belongs_to :attribution, class_name: 'Person', foreign_key: :attribution
   belongs_to :author, class_name: 'Person', foreign_key: :author
+  has_and_belongs_to_many :votes
 
   validates_presence_of :text
   validates_uniqueness_of :text, case_sensitive: false

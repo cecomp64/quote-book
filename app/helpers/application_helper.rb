54564e22aecc7af80dc6bc1f944d0f14a6c735ed
Carl Svensson <csvenss2@gmail.com>
2017-06-11 20:39:15 -0700

    Highlight search results

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def display_person(p)
     return p.name if(p)
   end
+
+  def highlight(str, query)
+    return str if(query.nil?)
+    str.gsub(/(#{query})/i, '<span class="highlight">\1</span>').html_safe
+  end
 end

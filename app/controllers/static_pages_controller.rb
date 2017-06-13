b3ed60e89e45ef653f47981d6e26cbea18b326d9
Carl Svensson <csvenss2@gmail.com>
2017-06-13 13:46:31 -0700

    Show top quoters

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,7 +7,7 @@ class StaticPagesController < ApplicationController
     @random_quote = MPQ.order('RANDOM()').first
     @top_quotes = MPQ.joins(:quotes).order(score: :desc).limit(sample_num)
     @recent_quotes = MPQ.joins(:quotes).order(created_at: :desc).limit(sample_num)
-    @top_quoters = Person.order(num_quotes: :desc).limit(sample_num)
+    @top_quoters = Person.where.not(num_quotes: nil).order(num_quotes: :desc).limit(sample_num)
 
   end
 end

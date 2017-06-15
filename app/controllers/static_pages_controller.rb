4173f55946de075d426340f01f081f7b3cfa4641
Carl Svensson <csvenss2@gmail.com>
2017-06-15 13:03:37 -0400

    Better MPQ style Make queries selet unique MPQs

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,8 +5,8 @@ class StaticPagesController < ApplicationController
     sample_num = 5
 
     @random_quote = MPQ.order('RANDOM()').first
-    @top_quotes = MPQ.joins(:quotes).order(score: :desc).limit(sample_num)
-    @recent_quotes = MPQ.joins(:quotes).order(created_at: :desc).limit(sample_num)
+    @top_quotes = MPQ.joins(:quotes).order(score: :desc).limit(sample_num).uniq
+    @recent_quotes = MPQ.joins(:quotes).order(created_at: :desc).limit(sample_num).uniq
     @top_quoters = Person.where.not(num_quotes: nil).order(num_quotes: :desc).limit(sample_num)
 
   end

4a3fddd7e37de3619bb289eb3abd2e3852693ca7
Carl Svensson <csvenss2@gmail.com>
2017-06-18 11:11:21 -0700

    Go back to previous page on login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :store_current_location, :unless => :devise_controller?
+
+  private
+  # override the devise helper to store the current location so we can
+  # redirect to it after loggin in or out. This override makes signing in
+  # and signing up work automatically.
+  def store_current_location
+    store_location_for(:user, request.url)
+  end
 end
